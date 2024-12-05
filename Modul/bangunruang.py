f30337c39d22aa20a41fc39ae93253de89ba85ca
Jaya Myzee <72689440+Jayapmks01@users.noreply.github.com>
2024-12-05 10:38:32 +0700

    Delete Modul directory

--- Modul/bangunruang.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def l_tabung(jari2, tinggi):
-    luas = 2*math.pi*(jari2 + tinggi)
-    print(f'Luas Tabung adalah = {luas}')
--- Modul/operator.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def tambah(bil1, bil2):
-    jumlah = bil1 + bil2
-    print(f'Hasil penjumlahan {bil1} + {bil2} = {jumlah}')
